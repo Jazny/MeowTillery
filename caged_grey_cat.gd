59be5e90e2e6b4862ad3fa17b6e04c9aadf9d107
Sonaz <89308555+Jazny@users.noreply.github.com>
2022-11-10 08:41:37 -0500

    Update caged_grey_cat.gd

--- caged_grey_cat.gd
+++ caged_grey_cat.gd
@@ -1,19 +1,31 @@
-extends Node2D
+extends TextureProgress
 
 func _ready():
-	get_node("grey_cat_chilling").play("default")
-	
+	value = 100
+	tint_progress = Color(0.26, 0.77, 0.19, 1)
+	get_node("Timer").wait_time = 200
+	get_node("Timer").start(get_node("Timer").wait_time)
 
-func _on_Button_pressed():
-	get_node("grey_cat_chilling").hide()
-	get_node("AnimationPlayer").play("Weapon Unlocked")
-
-
-func _on_Button2_pressed():
-	get_node("grey_cat_chilling").play("eating")
-	yield(get_tree().create_timer(2), "timeout")
-	get_node("grey_cat_chilling").play("default")
+func _process(delta):
+	value = (get_node("Timer").time_left / get_node("Timer").wait_time) * 100
+	if (value > 70):
+		tint_progress = Color(0.26, 0.77, 0.19, 1)
+	elif (value <= 70 && value > 50):
+		tint_progress = Color(0.88, 0.85, 0.21, 1)
+	elif (value <= 50 && value > 30):
+		tint_progress = Color(0.88, 0.65, 0.21, 1)
+	elif (value <= 30):
+		tint_progress = Color(0.88, 0.29, 0.21, 1)
 
 
 func _on_Timer_timeout():
-	get_node("grey_cat_chilling").play("death")
+	hide()
+	
+	
+func _on_Button_pressed():
+	hide()
+
+
+func _on_Button2_pressed():
+	if get_node("Timer").time_left < (get_node("Timer").wait_time - 20):
+		get_node("Timer").time_left+=20
