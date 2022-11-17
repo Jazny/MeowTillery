4120cb6d06b1873bf22586fbcddc7b6a53a6b3fc
Sean Kerzel <skerzel@vols.utk.edu>
2022-11-17 00:39:55 -0500

    padthai spawning

--- Pickup Zone.gd
+++ Pickup Zone.gd
@@ -10,7 +10,12 @@ var items_in_range = {}
 func _on_PickupZone_body_entered(body):
 	items_in_range[body] = body
 
-
 func _on_PickupZone_body_exited(body):
 	if items_in_range.has(body):
 		items_in_range.erase(body)
+
+
+func _on_PickupZone_area_entered(area):
+	if(area.name == "PtArea" and PlayerStat.health > 0):
+		PlayerStat._set_health(PlayerStat.health + 20)
+		area.get_parent().queue_free()
