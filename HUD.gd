ff1d78307f16f55f06cdb2bbb8bb26d4c84aa360
U-LAPTOP-U4L9DA93\conom <comalle2@vols.utk.edu>
2022-11-28 14:05:57 -0500

    Resolves #48

--- HUD.gd
+++ HUD.gd
@@ -1,9 +1,17 @@
 extends CanvasLayer
 
 
+var holding_item = null
+
+
 func _input(event):
 	if event.is_action_pressed("Inventory"):
 		$Inventory.visible = !$Inventory.visible
 		$Inventory.intialize_inventory()
+	
+	if event.is_action_pressed("scroll_up"):
+		PlayerInventory.active_item_scroll_down()
+	elif event.is_action_pressed("scroll_down"):
+		PlayerInventory.active_item_scroll_up()
 func _ready():
 	pass
