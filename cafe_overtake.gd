439fc9a8d5867d1417f3adb87e3308b7644ba5b9
Jazny <queenerweaner@gmail.com>
2022-12-02 00:15:18 -0500

    more

--- cafe_overtake.gd
+++ cafe_overtake.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 onready var objective = get_node("AnimationPlayer")
+onready var cam = get_node("Squango").get_node("Camera2D")
 
 func _ready():
+	cam.current = false
+	cam.anchor_mode = Camera2D.ANCHOR_MODE_FIXED_TOP_LEFT
 	objective.play("Objective")
 	yield(get_tree().create_timer(3), "timeout")
 	
