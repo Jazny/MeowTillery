dd7bc941e414f5f171f60e06dc40ebd7ec97cd1c
Sonaz <89308555+Jazny@users.noreply.github.com>
2022-12-03 14:31:28 -0500

    all starter characters are good to go

---
+++ calicoHungerBar.gd
@@ -0,0 +1,33 @@
+extends TextureProgress
+
+onready var catstats = get_node("/root/CatsGlobal")
+var inzone = false
+
+func _ready():
+	value = catstats.calico_cat_hunger
+	tint_progress = Color(0.26, 0.77, 0.19, 1)
+	get_node("Timer").wait_time = catstats.calico_cat_time_left
+	get_node("Timer").start(get_node("Timer").wait_time)
+
+func _process(delta):
+	value = get_node("Timer").time_left
+	catstats.calico_cat_time_left = get_node("Timer").time_left
+	catstats.calico_cat_hunger = value
+	if (value > 70):
+		tint_progress = Color(0.26, 0.77, 0.19, 1)
+	elif (value <= 70 && value > 50):
+		tint_progress = Color(0.88, 0.85, 0.21, 1)
+	elif (value <= 50 && value > 30):
+		tint_progress = Color(0.88, 0.65, 0.21, 1)
+	elif (value <= 30):
+		tint_progress = Color(0.88, 0.29, 0.21, 1)
+	
+	if Input.is_action_just_pressed("Feed") && inzone == true:
+		if (value < 90):
+			get_node("Timer").wait_time+=10
+			get_node("Timer").start(get_node("Timer").wait_time)
+
+
+func _on_Timer_timeout():
+	hide()
+
--- cat_dungeon.gd
+++ cat_dungeon.gd
@@ -5,3 +5,7 @@ onready var catstats = get_node("/root/CatsGlobal")
 func _ready():
 	if (catstats.grey_cat_alive == false):
 		get_node("caged_grey_cat").queue_free()
+	if (catstats.calico_cat_alive == false):
+		get_node("caged_calico_cat").queue_free()
+	if (catstats.black_cat_alive == false):
+		get_node("caged_black_cat").queue_free()
