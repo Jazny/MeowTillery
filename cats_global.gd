7a48584fbad50f2e424b7a655ea8c65e6d7987ec
Sonaz <89308555+Jazny@users.noreply.github.com>
2022-12-04 13:33:10 -0500

    um

--- cats_global.gd
+++ cats_global.gd
@@ -3,31 +3,39 @@ extends Node
 var grey_cat_alive
 var calico_cat_alive
 var black_cat_alive
+var pink_cat_alive
 
 var grey_cat_hunger
 var calico_cat_hunger
 var black_cat_hunger
+var pink_cat_hunger
 
 var grey_cat_time_left
 var calico_cat_time_left
 var black_cat_time_left
+var pink_cat_time_left
 
 var grey_cat_num_times_fed
 var calico_cat_num_times_fed
 var black_cat_num_times_fed
+var pink_cat_num_times_fed
 
 func _ready():
 	grey_cat_alive = true
 	calico_cat_alive = true
 	black_cat_alive = true
+	pink_cat_alive = true
 	grey_cat_hunger = 100
 	calico_cat_hunger = 100
 	black_cat_hunger = 100
+	pink_cat_hunger = 100
 	grey_cat_time_left = 100
 	calico_cat_time_left = 100
 	black_cat_time_left = 100
+	pink_cat_time_left = 100
 	grey_cat_num_times_fed = 0
 	calico_cat_num_times_fed = 0
 	black_cat_num_times_fed = 0
+	pink_cat_num_times_fed = 0
 	
 	
